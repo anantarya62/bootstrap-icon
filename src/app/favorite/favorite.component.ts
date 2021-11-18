ebb070710cd05ba21e27b879045103a820601ab9
Rohan Karn <karn.rohan1@gmail.com>
2021-11-18 13:34:24 +0530

    doubt

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,10 +1,9 @@
-import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css', '~/../../../styles.css']
+  styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
   
